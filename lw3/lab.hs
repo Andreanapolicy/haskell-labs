a030ebf078d6b466cf3696475bf0b042c8afbc84
Andreanapolicy <daa.and@mail.ru>
2024-01-28 15:52:00 +0300

    LW3. Добавил третье задание

--- lw3/lab.hs
+++ lw3/lab.hs
@@ -10,6 +10,15 @@ secondlastlist (x:xs) = if null (x)
                         then secondlastlist xs
                         else last x : secondlastlist xs
 
+-- 3 task
+myunion :: [Int] -> [Int] -> [Int]
+myunion list [] = list
+myunion [] list = list
+myunion list (x:xs) = if elem x list
+                      then myunion list xs
+                      else myunion (x : list) xs
+
 main = do
     print(listnums 10)
     print(secondlastlist [[1, 2], [1, 2, 3], [3, 4, 5, 6]])
+    print(myunion [2, 3] [3, 2])
