c7cbc4923338c479753c814305af573e85c9649c
Andreanapolicy <daa.and@mail.ru>
2024-01-28 15:45:23 +0300

    LW3. Добавил второе задание

--- lw3/lab.hs
+++ lw3/lab.hs
@@ -3,6 +3,13 @@ listnums :: Int -> [Int]
 listnums 1 = [1]
 listnums n = if n < 1 then [-1] else (n : listnums(n - 1))
 
+-- 2 task
+secondlastlist :: [[Int]] -> [Int]
+secondlastlist [] = []
+secondlastlist (x:xs) = if null (x)
+                        then secondlastlist xs
+                        else last x : secondlastlist xs
 
 main = do
     print(listnums 10)
+    print(secondlastlist [[1, 2], [1, 2, 3], [3, 4, 5, 6]])
