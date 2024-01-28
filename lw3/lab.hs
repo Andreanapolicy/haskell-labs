477cbb7a67b57cafdd5ea9165cba9faa65f54945
Andreanapolicy <daa.and@mail.ru>
2024-01-28 15:57:32 +0300

    LW3. Добавил четвертое задание

--- lw3/lab.hs
+++ lw3/lab.hs
@@ -1,3 +1,5 @@
+import Data.List (delete)
+
 -- 1 task
 listnums :: Int -> [Int]
 listnums 1 = [1]
@@ -18,7 +20,18 @@ myunion list (x:xs) = if elem x list
                       then myunion list xs
                       else myunion (x : list) xs
 
+-- 4 task
+
+mysubst :: [Int] -> [Int] -> [Int]
+mysubst list [] = list
+mysubst [] list = []
+mysubst list (x:xs) = if elem x list
+                      then mysubst (delete x list) (x:xs)
+                      else mysubst (list) xs
+
+
 main = do
     print(listnums 10)
     print(secondlastlist [[1, 2], [1, 2, 3], [3, 4, 5, 6]])
     print(myunion [2, 3] [3, 2])
+    print(mysubst [2, 1] [1])
