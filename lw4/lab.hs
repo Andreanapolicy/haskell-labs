3ee19063f54287e159eca66f5a9ce7af5b410c14
Andreanapolicy <daa.and@mail.ru>
2024-02-04 15:26:08 +0300

    LW4. Сделал 2 задание

--- lw4/lab.hs
+++ lw4/lab.hs
@@ -11,5 +11,26 @@ myintersperse _ [x] = [x]
 myintersperse separator (x:xs) = x : separator : myintersperse separator xs
 
 
+-- Update a value at a specific key with the result of the provided function.
+-- When the key is not
+-- a member of the map, the original map is returned.
+--
+-- > adjust ("new " ++) 5 (fromList [(5,"a"), (3,"b")]) == fromList [(3, "b"), (5, "new a")]
+-- > adjust ("new " ++) 7 (fromList [(5,"a"), (3,"b")]) == fromList [(3, "b"), (5, "a")]
+-- > adjust ("new " ++) 7 empty                         == empty
+-- Проверяем индекс, если он невалиден, возвращаем список. 
+-- Иначе разбиваем список на 2 части по индексу, и формируем новый: 1 часть, обработка головы второй части, хвост 2 части
+myadjust :: (a -> a) -> Int -> [a] -> [a]
+myadjust function index list = 
+  if index < 0 || index >= length list
+    then list
+  else 
+    let (before, after) = splitAt index list
+    in before ++ [function (head after)] ++ tail after
+
 main = do
     print(myintersperse '-' "123")
+
+    print(myadjust (\x -> x + 1) 2 [1, 2, 3, 4, 5])
+    print(myadjust (\_ -> "hello") 0 ["world", "apple", "orange"])
+    
