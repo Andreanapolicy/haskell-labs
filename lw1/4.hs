84bc5712ac323d5f55b337e59e839880914b92af
Andreanapolicy <daa.and@mail.ru>
2024-02-04 14:47:00 +0300

    LW4. Начал делать лабы. Создал файлик и описание

--- lw1/4.hs
+++ lw1/4.hs
@@ -1,5 +1,8 @@
 getTriangleNumbers :: Int -> [Int]
-getTriangleNumbers count = [sum[1..x] | x <- [1..count]]
+getTriangleNumbers count = [sum[1..x] | x^2 <- [1..count]]
+
+getPyramidalNumbers :: Int -> [Int]
+getPyramidalNumbers count = [sum(getTriangleNumbers x) | x^2 <- [1..count]]
 
 main :: IO ()
 main = do
---
+++ lw4/readme.md
@@ -0,0 +1,6 @@
+Написать функции:
+- `intersperse` из `Data.List`
+- `adjust` из `Data.Map`
+- `unlines` из `Data.String`
+- `cis` из `Data.Complex`
+- `range` из `Data.Ix`
