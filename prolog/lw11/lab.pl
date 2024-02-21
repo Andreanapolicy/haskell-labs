810b730474a5cf84940538aad33228f07e4be538
Andreanapolicy <daa.and@mail.ru>
2024-02-21 22:06:50 +0300

    LW11. Сделал 3, 4, 5, 6 задания

--- prolog/lw11/lab.pl
+++ prolog/lw11/lab.pl
@@ -12,6 +12,29 @@ first_last([H|T], L2) :-
 
 % --- Task 3 ---
 
+total([], 1). 
+total([X|XS], P) :- 
+    total(XS, P1), 
+    P is X * P1.
 
 % --- Task 4 ---
 
+place(X, L, [X|L]).
+place(X, [H|T], [H|L]) :- 
+    place(X, T, L).
+
+% --- Task 5 ---
+
+double([], []).
+double([H|T], [H, H | T2]) :- 
+    double(T, T2).
+
+% --- Task 6 ---
+
+split([], [], []).
+split([H|T], L2, [H|L3]) :- 
+    0 is H mod 2, 
+    split(T, L2, L3).
+split([H|T], [H|L2], L3) :- 
+    1 is H mod 2, 
+    split(T, L2, L3).
